ebfe85352f00cefc0d943e533c26b588eb0ae145
Yuji Sugiura <y.sugiura.0316@gmail.com>
2020-04-10 21:40:41 +0900

    Mutable history

--- src/utils/history.js
+++ src/utils/history.js
@@ -1,6 +1,6 @@
 /**
  *
- * History for undo/redo Structure
+ * History for undo/redo Structure(mutable)
  * See `https://gist.github.com/leader22/9fbed07106d652ef40fda702da4f39c4`
  *
  */
@@ -18,28 +18,24 @@ export class History {
   undo() {
     if (this._past.length === 0) return;
 
-    const previous = this._past[this._past.length - 1];
-    const newPast = this._past.slice(0, this._past.length - 1);
-    this._past = newPast;
-    this._future = [this._present, ...this._future];
+    const previous = this._past.pop();
+    this._future.unshift(this._present);
     this._present = previous;
   }
 
   redo() {
     if (this._future.length === 0) return;
 
-    const next = this._future[0];
-    const newFuture = this._future.slice(1);
-    this._past = [...this._past, this._present];
-    this._future = newFuture;
+    const next = this._future.shift();
+    this._past.push(this._present);
     this._present = next;
   }
 
   save(newPresent) {
     if (this._present === newPresent) return;
 
-    this._past = [...this._past, this._present];
-    this._future = [];
+    this._past.push(this._present);
+    this._future.length = 0;
     this._present = newPresent;
   }
 
