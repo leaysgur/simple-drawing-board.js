d4f2d73432d405c4b3988c86717d859e2b526890
Yuji Sugiura <y.sugiura.0316@gmail.com>
2020-04-10 11:52:39 +0900

    Impl history

--- __tests__/utils/utils.test.js
+++ __tests__/utils/utils.test.js
@@ -1,40 +1,9 @@
-import {
-  isTouch,
-  isTransparent,
-  isDrawableElement,
-} from "../../src/utils/utils";
+import { isTouch, isDrawableElement } from "../../src/utils/utils";
 
 describe("isTouch", () => {
   it("should return false", () => {
     // in ChromeHeadless
     expect(isTouch()).toBeFalsy();
-  });
-});
-
-describe("isTransparent", () => {
-  it("should return true", () => {
-    const colors = [
-      "transparent",
-      "rgba(0,0,0,0)",
-      "rgba( 1, 2, 3, 0 )",
-      "hsl(10%, 20%, 30%, 0)",
-    ];
-    for (const color of colors) {
-      expect(isTransparent(color)).toBeTruthy();
-    }
-  });
-
-  it("should return false", () => {
-    const colors = [
-      "",
-      "orange",
-      "rgb(1,2,3)",
-      "rgba(0,0,0,0.1)",
-      "hsla(3,4,5,1)",
-    ];
-    for (const color of colors) {
-      expect(isTransparent(color)).toBeFalsy();
-    }
   });
 });
 
---
+++ src/utils/history.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * History for undo/redo Structure
+ *
+ *     past: [0, 1, 2, 3, 4, 5, 6, 7],
+ *  present: 8,
+ *   future: [9, 10]
+ *
+ */
+export class History {
+  constructor() {
+    this._past = [];
+    this._present = null;
+    this._future = [];
+  }
+
+  get value() {
+    return this._present;
+  }
+
+  undo() {
+    if (this._past.length === 0) return;
+
+    const previous = this._past[this._past.length - 1];
+    const newPast = this._past.slice(0, this._past.length - 1);
+    this._past = newPast;
+    this._future = [this._present, ...this._future];
+    this._present = previous;
+  }
+
+  redo() {
+    if (this._future.length === 0) return;
+
+    const next = this._future[0];
+    const newFuture = this._future.slice(1);
+    this._past = [...this._past, this._present];
+    this._future = newFuture;
+    this._present = next;
+  }
+
+  save(newPresent) {
+    if (this._present === newPresent) return;
+
+    this._past = [...this._past, this._present];
+    this._future = [];
+    this._present = newPresent;
+  }
+}
