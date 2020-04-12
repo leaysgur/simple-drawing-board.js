a6598eeb74401ae1501e433000de6590595d0cdb
Yuji Sugiura <y.sugiura.0316@gmail.com>
2020-04-12 12:38:20 +0900

    Build

--- dist/simple-drawing-board.js
+++ dist/simple-drawing-board.js
@@ -62,7 +62,7 @@
 
   /**
    *
-   * History for undo/redo Structure
+   * History for undo/redo Structure(mutable)
    * See `https://gist.github.com/leader22/9fbed07106d652ef40fda702da4f39c4`
    *
    */
@@ -80,28 +80,24 @@
     undo() {
       if (this._past.length === 0) return;
 
-      const previous = this._past[this._past.length - 1];
-      const newPast = this._past.slice(0, this._past.length - 1);
-      this._past = newPast;
-      this._future = [this._present, ...this._future];
+      const previous = this._past.pop();
+      this._future.unshift(this._present);
       this._present = previous;
     }
 
     redo() {
       if (this._future.length === 0) return;
 
-      const next = this._future[0];
-      const newFuture = this._future.slice(1);
-      this._past = [...this._past, this._present];
-      this._future = newFuture;
+      const next = this._future.shift();
+      this._past.push(this._present);
       this._present = next;
     }
 
     save(newPresent) {
       if (this._present === newPresent) return;
 
-      this._past = [...this._past, this._present];
-      this._future = [];
+      this._past.push(this._present);
+      this._future.length = 0;
       this._present = newPresent;
     }
 
@@ -251,7 +247,7 @@
       return this._ctx.canvas.toDataURL(type, quality);
     }
 
-    setImageByElement($el) {
+    fillImageByElement($el) {
       if (!isDrawableElement($el))
         throw new TypeError("Passed element is not a drawable!");
 
@@ -262,7 +258,7 @@
       this._saveHistory();
     }
 
-    async setImageByDataURL(src) {
+    async fillImageByDataURL(src) {
       if (!isBase64DataURL(src))
         throw new TypeError("Passed src is not a base64 data URL!");
 
