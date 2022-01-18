04b8d285ec229783ac479a7ee9434f3d19dcb66a
Yuji Sugiura <y.sugiura.0316@gmail.com>
2022-01-18 13:39:19 +0900

    Update deps

--- dist/simple-drawing-board.js
+++ dist/simple-drawing-board.js
@@ -2,7 +2,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.SimpleDrawingBoard = {}));
-}(this, (function (exports) { 'use strict';
+})(this, (function (exports) { 'use strict';
 
   /**
    *
@@ -430,4 +430,4 @@
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
-})));
+}));
